e59bab82d0a0ab10112d5f0e295a35f674b178ea
achansek <36383039+achansek@users.noreply.github.com>
2023-03-08 14:50:26 -0500

    Updated app.py
    tested on docker.

--- project/app/app.py
+++ project/app/app.py
@@ -8,24 +8,23 @@ import base64
 import numpy as np
 # Create dash app
 app = dash.Dash(__name__)
+server = app.server
 # Generate dataframe
 kmdf = pd.read_csv('Fgrow10_3_kmdf.csv',index_col=0)
-print(kmdf)
-# Create scatter plot with x and y coordinates
+
 fig = px.scatter(kmdf, x="Actin occupied Surface Fraction", 
                  y="Mean number of unique actin filaments per VASP",
                  custom_data=["images"],color="Predicted Cluster",
                  labels ={
-               "Mean number of unique actin filaments per VASP":
+                 "Mean number of unique actin filaments per VASP":
                  'Mean number of unique <br>  actin filaments per VASP',
                  },)
-
 fig.update_layout(
-    font=dict(
-        family="Arial",
-        size=18,  # Set the font size here
-        color="Black"
-    )
+   font=dict(
+      family="Arial",
+      size=18,  # Set the font size here
+      color="Black"
+   )
 )
 
 # Update layout and update traces
@@ -42,6 +41,7 @@ app.layout = html.Div(
       html.Img(id='image', src='')
    ]
 )
+
 
 # html callback function to hover the data on specific coordinates
 @app.callback(
